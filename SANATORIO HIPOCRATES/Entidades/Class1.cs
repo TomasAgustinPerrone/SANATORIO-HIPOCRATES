5cb80b3eae4c05869493a1866059a825f04c291e
Santiago Grinblat <grinblatsantiago@gmail.com>
2023-06-17 21:48:27 -0300

    primera entidad

---
+++ SANATORIO HIPOCRATES/Entidades/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SANATORIO_HIPOCRATES.Entidades
+{
+    internal class Persona
+    {
+        long id;
+    }
+}
