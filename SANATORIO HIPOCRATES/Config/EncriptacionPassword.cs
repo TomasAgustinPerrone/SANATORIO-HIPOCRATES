05fe4dcfd3baaf974c00957fc4770ef958e02880
Matias Gonzalez <matias.r.gozalez@gmail.com>
2023-06-22 23:23:01 -0300

    Comentario de prueba en encriptado

--- SANATORIO HIPOCRATES/Config/EncriptacionPassword.cs
+++ SANATORIO HIPOCRATES/Config/EncriptacionPassword.cs
@@ -9,6 +9,7 @@ namespace SANATORIO_HIPOCRATES.Config
 {
     public static class EncriptacionPassword
     {
+        //Encripta la contraseña en protocolo SHA256 
         public static string GenerarHash(string password)
         {
             using (SHA256 sha256Hash = SHA256.Create())
@@ -25,6 +26,7 @@ namespace SANATORIO_HIPOCRATES.Config
         }
 
 
+        //Verifica que sea la contraseña esperada en la bbdd
         public static bool VerificarPassword(string passwordIngresada, string hashAlmacenado)
         {
             string hashPasswordIngresada = GenerarHash(passwordIngresada.Trim() );
